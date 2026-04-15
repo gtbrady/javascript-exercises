6364e54576dc70c68874f92ee9f6a163ab70b50e
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-04-15 16:44:37 -0500

    Solve 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...number) {
+    let result = [];
+    for(let i = 0; i < array.length; i++) {
+        if(!number.includes(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
