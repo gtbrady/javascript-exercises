ec89209f696e8a158e102b0da06753871e2bea66
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-04-15 23:12:18 -0500

    Solve 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(first,last) {
+    if(!(first >= 0 && Number.isInteger(first) && last >= 0 && Number.isInteger(last))) {
+        return "ERROR";
+    }
+    const min = first <= last ? first : last;
+    const max = first >= last ? first : last;
+    let sum = 0;
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
