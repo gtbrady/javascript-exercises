691a277271fec75ae280648efed39106797e963b
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-04-16 00:09:09 -0500

    Solve 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+/* 
+Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa
+
+to Fahrenheit
+(0°C × 9/5) + 32 = 32°F
+
+to Celsius
+(0°F − 32) × 5/9 = -17.78°C 
+*/
+
+
+const convertToCelsius = function(fahrenheit) {
+  const conversion = (fahrenheit - 32) * (5/9);
+  return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const conversion = (celsius * (9/5)) + 32;
+  return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
