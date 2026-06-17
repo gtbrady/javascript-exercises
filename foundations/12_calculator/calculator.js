9159b19009f8e8aa3a321066e2913a720d089252
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-06-17 09:35:53 -0500

    Solve 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((acc, current) => acc += current, 0);
 };
 
-const multiply = function() {
 
+
+const multiply = function(numbers) {
+  return numbers.reduce((acc, current) => acc *= current);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+  return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	result = 1;
+  for(let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
