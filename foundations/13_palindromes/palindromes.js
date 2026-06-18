cd996c46d9eec42965e7f3db86eafcf041d2d70d
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-06-18 09:37:13 -0500

    Solve 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+
+    
+    const cleanedString = word
+                    .toLowerCase()
+                    .split("")
+                    .filter((char) => alphaNumeric.includes(char))
+                    .join("");
+    //'acaramanamaraca'
+    const reversedString =  cleanedString
+                            .split("")
+                            .reverse()
+                            .join("");
+    return cleanedString === reversedString;
 
 };
 
