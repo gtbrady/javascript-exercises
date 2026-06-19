a067c66652e098e511f4f80f81355cccf44e7c80
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-06-19 08:44:39 -0500

    Solve 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    //check if num is int; parse int if not
+    let seq;
+    if(typeof(num) !== "number") {
+        seq = Number.parseInt(num);
+    } else {
+        seq = num;
+    }
+
+    //check for negative
+    if(seq < 0) return "OOPS";
+    //check for 0; return 0
+    if(seq === 0) return seq;
+
+    //left being the leftmost # in the fib sequence, when adding the number pairs
+    let left = 0;
+    let right = 1;
+
+    for(let i = 2; i <= seq; i++) {
+        let current = left + right;
+        left = right;
+        right = current;
+    }
+    return right;
 
 };
 
