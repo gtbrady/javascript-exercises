96d04095ea4cff4280783234224add4471a0f988
gtbrady <97182053+gtbrady@users.noreply.github.com>
2026-06-20 10:04:56 -0500

    Solve 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+function findAge(birth, death) {
+    return (!death ? new Date().getFullYear() : death) - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = findAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge > currentAge ? oldest : current;
+    });
 };
 
 // Do not edit below this line
